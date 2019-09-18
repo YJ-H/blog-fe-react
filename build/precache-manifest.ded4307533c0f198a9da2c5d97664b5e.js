cc9bf3e2befdc4d4d10d5a9b4f0ac0ff63194200
YJ-H <a910102423@gmail.com>
2019-09-18 20:57:34 +0800

    update

---
+++ build/precache-manifest.ded4307533c0f198a9da2c5d97664b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde31279479fa18a5d2acf9a89acfec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2204d5b79b9c0c31c15a",
+    "url": "/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "84285fea5ca5650bb6c7",
+    "url": "/static/css/main.4e462057.chunk.css"
+  },
+  {
+    "revision": "2204d5b79b9c0c31c15a",
+    "url": "/static/js/2.c3e187e9.chunk.js"
+  },
+  {
+    "revision": "84285fea5ca5650bb6c7",
+    "url": "/static/js/main.d645fce1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd008bfd234f157acab6b9da60375008",
+    "url": "/static/media/login.fd008bfd.jpg"
+  }
+]);
